e726313237b544b2fb821969ca2c6612672403e5
jungjinhwan0101 <jungjinhwan0101@gmail.com>
2020-11-18 00:23:22 +0900

    표현계층 : main

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-from database import DatabaseManager
+import commands
 
 
-db = DatabaseManager('test.db')
-columns = {
-    'id': 'INTEGER PRIMARY KEY AUTOINCREMENT',
-    'title': 'TEXT NOT NULL',
-    'url': 'TEXT NOT NULL',
-    'notes': 'TEXT',
-    'date_added': 'TEXT NOT NULL'
-}
-db.create_table('test', columns)
-db.add('test', {'title': 'test_title', 'url': 'www.test.com'})
-cursor = db.select('test', {'title': 'test_title'})
-print(cursor.fetchall())
-db.delete('test', {'title': 'test_title'})
+if __name__ == '__main__':
+    commands.CreateBookmarksTableCommand().execute()
