2e882c47d5f8f39fd0f6414c3eb2762cd6e62acb
jungjinhwan0101 <jungjinhwan0101@gmail.com>
2020-11-18 00:21:49 +0900

    비즈니스 로직계층 작성

---
+++ commands.py
@@ -0,0 +1,45 @@
+import sys
+from datetime import datetime
+from database import DatabaseManager
+
+
+db = DatabaseManager('bookmarks.db')
+
+
+class CreateBookmarksTableCommand:
+    def execute(self):
+        db.create_table(
+            'bookmarks', {
+                'id': 'INTEGER PRIMARY KEY AUTOINCREMENT',
+                'title': 'TEXT NOT NULL',
+                'url': 'TEXT NOT NULL',
+                'notes': 'TEXT',
+                'date_added': 'TEXT NOT NULL'
+            }
+        )
+
+
+class AddBookmarkCommand:
+    def execute(self, data):
+        data['date_added'] = datetime.utcnow().isoformat()
+        db.add('bookmarks', data)
+        return 'Bookmark added!'
+
+
+class ListBookmarksCommand:
+    def __init__(self, order_by='date_added'):
+        self.order_by = order_by
+
+    def execute(self):
+        return db.select('bookmarks', order_by=self.order_by).fetchall()
+
+
+class DeleteBookmarkCommand:
+    def execute(self, data):
+        db.delete('bookmarks', {'id': data})
+        return 'Bookmark deleted!'
+
+
+class QuitCommand:
+    def execute(self):
+        sys.exit()
--- main.py
+++ main.py
@@ -6,8 +6,11 @@ columns = {
     'id': 'INTEGER PRIMARY KEY AUTOINCREMENT',
     'title': 'TEXT NOT NULL',
     'url': 'TEXT NOT NULL',
-    'notes': 'TEXT'
+    'notes': 'TEXT',
+    'date_added': 'TEXT NOT NULL'
 }
 db.create_table('test', columns)
 db.add('test', {'title': 'test_title', 'url': 'www.test.com'})
+cursor = db.select('test', {'title': 'test_title'})
+print(cursor.fetchall())
 db.delete('test', {'title': 'test_title'})
